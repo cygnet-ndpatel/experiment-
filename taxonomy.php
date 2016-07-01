6f7d63d24cfcf0c0fad007cf0e8c30f77b7af5e0
cygnet-ndpatel <cygnet.ndpatel@gmail.com>
2016-07-01 17:22:45 +0530

    taxonomy final

--- taxonomy.php
+++ taxonomy.php
@@ -1,90 +1,46 @@
 <?php
+	//global $taxonomyName, $customTaxName;
+	$taxonomyName = 'writer';
+	$customTaxName = 'category';
 
-$terms = get_terms('seller_category' , array(
- 	'orderby'    => 'none',
- 	'hide_empty' => 0
-));
-
-if( ! empty( $terms ) && ! is_wp_error( $terms ) )
-{
-   foreach ( $terms as $term )
-   {
-
-
-$catTerms = get_terms('category' , array(
- 	'orderby'    => 'none',
- 	'hide_empty' => 0
-));
-
-foreach ( $catTerms as $cams )
-{
-$flag = 0;
-if( $cams->name == $term->name )
-{
-$flag = 1;
-}
-}
-if($flag == 0)
-{
-            $termchildren = get_term_children($term->term_id, 'seller_category');
-            if( ! empty( $termchildren ))
-	    {	    	
-                $insert_id = wp_insert_term( $term->name, 'category');
-	    	foreach ( $termchildren as $child )
-                {
-				$childterm = get_term_by( 'id', $child, 'seller_category' );
-				$my_cat = array( 'parent'=> $insert_id['term_id'] );
-				$insert_child_id = wp_insert_term( $childterm->name, 'category', $my_cat );
-                                $termsubchildren = get_term_children($insert_child_id, 'seller_category');
-                                if( ! empty( $termchildren ))
-	                        {
-addchild($termsubchildren, $insert_child_id);
-}
+	$customTaxTerms = get_terms($taxonomyName , array(
+					 	'orderby'    => 'none',
+					 	'hide_empty' => 0,
+					 	'parent'     => 0
+					));
+	if(! empty( $customTaxTerms ) && ! is_wp_error( $customTaxTerms ))
+	{
+		foreach ( $customTaxTerms as $term )
+		{
+			$insert_id = wp_insert_term( $term->name, $customTaxName);
+			$customTaxtChildTerm = get_terms($taxonomyName , array(
+					 	'orderby'    => 'none',
+					 	'hide_empty' => 0,
+					 	'parent'     => $term->term_id
+					));
+			if( ! empty( $customTaxtChildTerm ))
+			{
+				addchild($customTaxtChildTerm, $insert_id, $taxonomyName, $customTaxName);
 			}
+		}
+	}
 
-	    
-	    }
-	    else
-	    {
-	    	
-$alldetail =  get_term_by( 'id', $term->term_id , 'seller_category' );
-if($alldetail->parent)
+function addchild($childarray, $parentId, $taxonomyName, $customTaxName)
 {
-$xyz = addparent($childterm->parent);
-
+	foreach($childarray as $childTerms)
+    {
+		$childterm = get_term_by( 'id', $childTerms->term_id, $taxonomyName );
+		$my_cat = array( 'parent'=> $parentId['term_id'] );
+		$insert_child_id = wp_insert_term( $childterm->name, $customTaxName, $my_cat );
+		$termsubchildren = get_terms($taxonomyName , array(
+				 	'orderby'    => 'none',
+				 	'hide_empty' => 0,
+				 	'parent'     => $childterm->term_id
+				));
+	    if( ! empty( $termsubchildren ))
+		{
+			addchild($termsubchildren, $insert_child_id, $taxonomyName, $customTaxName);
+		}
+	}
 }
-	    }
-    }
-}
-}
-
-function addparent($parent)
-{
-$ccterm = get_term( $parent, 'seller_category' );
-$my_cat = array( 'parent'=> $parent );
-$insert_child_ = wp_insert_term( $ccterm->name, 'category' );
-
-$subchildterm = get_term_by( 'id', $parent , 'seller_category' );
-if($subchildterm->parent)
-{
-    addparent($subchildterm->parent);
-}
-else
-{
-return false;
-}
-}
-
-function addchild($childarray, $parentId)
-{
-$childterm = get_term_by( 'id', $childarray, 'seller_category' );
-$my_cat = array( 'parent'=> $parentId['term_id'] );
-				$insert_child_id =wp_insert_term( $childterm->name, 'category', $my_cat );
-                                $termsubchildren = get_term_children($insert_child_id, 'seller_category');
-                                if( ! empty( $termchildren ))
-	                        {
-addchild($termsubchildren, $insert_child_id );
-}
-}
-
 ?>
